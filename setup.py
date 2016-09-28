afca87f667ea3e7c262206161534ed314f7f3342
polecito.em@gmail.com <polecito.em@gmail.com>
2016-09-28 10:34:14 -0700

    Adding LICENCe and fixing setup file

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
-  name = 'beam-utils',
+  name = 'beam_utils',
   packages = ['beam_utils'],
   version = '0.0.1',
   description = 'Small lib with a few utilities for the Python SDK of Apache Beam',
   author = 'Pablo E.',
   author_email = 'mail@iampablo.me',
   url = 'https://github.com/pabloem/',
-  download_url = 'https://github.com/pabloem/beam-utils/tarball/0.0.1',
+  download_url = 'https://github.com/pabloem/beam_utils/tarball/0.0.1',
   keywords = ['beam', 'dataflow', 'apache beam'],
   classifiers = [],
 )
