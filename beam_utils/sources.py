d61ce968e03c579a9fb2b1b4e8a2263842947885
polecito.em@gmail.com <polecito.em@gmail.com>
2016-10-04 11:49:37 -0700

    Adding JsonLinesFileSource

--- beam_utils/sources.py
+++ beam_utils/sources.py
@@ -1,6 +1,45 @@
 # Reads a CSV file
-import apache_beam as beam
+
 import csv
+from json import JSONDecoder
+from functools import partial
+
+import apache_beam as beam
+
+__all__ = ['JsonLinesFileSource', 'CsvFileSource']
+
+class JsonLinesFileSource(beam.io.filebasedsource.FileBasedSource):
+
+  def __init__(self, *args, **kwargs):
+    """ Initialize a JsonLinesFileSource.
+    """
+
+    if kwargs.get('splittable', False):
+      raise ValueError('JsonLines files can not be split arbitrarily')
+    # Can't just split anywhere
+    kwargs['splittable'] = False
+    super(self.__class__, self).__init__(*args, **kwargs)
+
+  def read_records(self, file_name, range_tracker):
+    self._file = self.open_file(file_name)
+
+    for rec in self._json_parse(self._file):
+      yield rec
+
+  # Routine from:
+  # stackoverflow/questions/21708192/how-do-i-use-the-json-module-to-read-in-one-json-object-at-a-time/
+  def _json_parse(self, fileobj, decoder=JSONDecoder(), buffersize=2048):
+    buffer = ''
+    for chunk in iter(partial(fileobj.read, buffersize), ''):
+         buffer += chunk
+         while buffer:
+             try:
+                 result, index = decoder.raw_decode(buffer)
+                 buffer = buffer[index:].lstrip() # Stripping new lines
+                 yield result
+             except ValueError:
+                 # Not enough data to decode, read more
+                 break
 
 
 class CsvFileSource(beam.io.filebasedsource.FileBasedSource):
@@ -8,8 +47,6 @@ class CsvFileSource(beam.io.filebasedsource.FileBasedSource):
 
   Parses a text file assuming newline-delimited lines,
   and comma-delimited fields. Assumes UTF-8 encoding.
-  It's completely based on the _TextSource.
-  **Note**: It can't parse files with newlines within a field.
   """
 
   def __init__(self, *args, **kwargs):
