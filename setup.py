886b8a91da7c552e9ce95bb6abaedc172b21841f
polecito.em@gmail.com <polecito.em@gmail.com>
2016-10-04 11:52:13 -0700

    Fixing for 0.0.2

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'beam_utils',
   packages = ['beam_utils'],
-  version = '0.0.1',
+  version = '0.0.2',
   description = 'Small lib with a few utilities for the Python SDK of Apache Beam',
   author = 'Pablo E.',
   author_email = 'mail@iampablo.me',
   url = 'https://github.com/pabloem/',
-  download_url = 'https://github.com/pabloem/beam_utils/tarball/0.0.1',
+  download_url = 'https://github.com/pabloem/beam_utils/tarball/0.0.2',
   keywords = ['beam', 'dataflow', 'apache beam'],
   classifiers = [],
 )
